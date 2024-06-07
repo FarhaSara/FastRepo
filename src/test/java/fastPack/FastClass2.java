cb54c33ca6412f6813ea72aca5d034cfa748a651
FarhaSara <farhatmulla9955@gmail.com>
2024-06-08 00:17:07 +0530

    Branch1 commit one

---
+++ src/test/java/fastPack/FastClass2.java
@@ -0,0 +1,27 @@
+package fastPack;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class FastClass2 {
+	
+	@Test
+	public void fast() {
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver=new ChromeDriver();
+		driver.get("https://www.google.com/");
+		
+		try {
+			Thread.sleep(10000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		driver.close();
+		
+	}
+
+}
